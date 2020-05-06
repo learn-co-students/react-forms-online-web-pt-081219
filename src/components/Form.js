044e805dd6d9db6db2998237ebffb11dc637c876
Alec Hall <alecnhall@gmail.com>
2020-05-06 17:41:35 -0600

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,68 @@
-import React from 'react';
+import React from "react";
 
 class Form extends React.Component {
   state = {
     firstName: "John",
-    lastName: "Henry"
-  }
+    lastName: "Henry",
+    submittedData: [],
+  };
 
-  handleFirstNameChange = event => {
+  handleFirstNameChange = (event) => {
     this.setState({
-      firstName: event.target.value
-    })
-  }
+      firstName: event.target.value,
+    });
+  };
 
-  handleLastNameChange = event => {
+  handleLastNameChange = (event) => {
     this.setState({
-      lastName: event.target.value
-    })
-  }
+      lastName: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let formData = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+    };
+    let dataArray = this.state.submittedData.concat(formData);
+    this.setState({
+      submittedData: dataArray,
+    });
+  };
+
+  listOfSubmissions = () => {
+    return this.state.submittedData.map((data) => {
+      return (
+        <div>
+          <span>{data.firstName} </span>
+          <span>{data.lastName}</span>
+        </div>
+      );
+    });
+  };
 
   render() {
     return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
-      </form>
-    )
+      <div>
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <input
+            type="text"
+            name="firstName"
+            onChange={(event) => this.handleFirstNameChange(event)}
+            value={this.state.firstName}
+          />
+          <input
+            type="text"
+            name="lastName"
+            onChange={(event) => this.handleLastNameChange(event)}
+            value={this.state.lastName}
+          />
+          <input type="submit" />
+        </form>
+        {this.listOfSubmissions()}
+      </div>
+    );
   }
 }
 
